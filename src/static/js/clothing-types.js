9435749a6bda797a7db4445aac4f500ea6b8171d
Hardik Sharma <hardik9462@gmail.com>
2026-08-28 17:20:48 +0530

    Improve dynamic item type filtering

---
+++ src/static/js/clothing-types.js
@@ -0,0 +1,37 @@
+const categoryTypes = Object.freeze({
+    "Tops": [
+        "T-Shirt",
+        "Shirt",
+        "Blouse",
+        "Sweater",
+        "Hoodie"
+    ],
+    "Bottoms": [
+        "Jeans",
+        "Trousers",
+        "Shorts",
+        "Skirt"
+    ],
+    "One-Piece": [
+        "Dress",
+        "Jumpsuit"
+    ],
+    "Outerwear": [
+        "Jacket",
+        "Coat",
+        "Blazer"
+    ],
+    "Shoes": [
+        "Sneakers",
+        "Boots",
+        "Sandals",
+        "Formal Shoes"
+    ],
+    "Accessories": [
+        "Bag",
+        "Scarf",
+        "Hat",
+        "Belt",
+        "Jewelry"
+    ]
+});
